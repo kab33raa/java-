734412bd92af75d4df26d2865860646b300add4b
kab33raa <kabeerbhowmik@gmail.com>
2026-01-19 14:32:11 +0530

    Add Mavenproject1 to calculate sum of two numbers

---
+++ program 1.java
@@ -0,0 +1,17 @@
+public class Mavenproject1 {
+
+   
+    public static void main(String[] args) {
+         int a=5,b=6,c=7;
+          System.out.println("Enter first number:");
+          Scanner s=new Scanner(System.in);
+          a=s.nextInt();
+          System.out.println("Enter Second number:");
+          b=s.nextInt();
+          
+          c=a+b;
+          
+          System.out.println("SUM: " +c);
+          
+    }
+}
